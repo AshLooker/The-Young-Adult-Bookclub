c963e33b170c03e4031c2715589cb2ef4f7608ea
AshLooker <Ash.looker14@gmail.com>
2022-09-22 17:13:06 +0000

    add interactive navbar

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav-mobile").style.width = "200px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav-mobile").style.width = "0";
+    document.body.style.backgroundColor = "white";
+  
+  }
