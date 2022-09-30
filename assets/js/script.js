ac0f71fa2488de9dd99aec835068569b72c2c406
AshLooker <Ash.looker14@gmail.com>
2022-09-30 14:25:24 +0000

    add js for passwords

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,16 @@ function openNav() {
     document.body.style.backgroundColor = "white";
   
   }
+
+var password = document.getElementById("password"), confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
